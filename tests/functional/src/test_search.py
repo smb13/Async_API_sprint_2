ed9b1f677621e56b1e25cd6da63c77dbb26243f7
Федор Зоркий <fedor.zorky@nanosemantics.ai>
2023-12-23 17:24:31 +0300

    Универсализация фикстуры запроса.

--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -31,15 +31,15 @@ ES_MOVIES_TEST_DATA = [{
 
 @pytest.mark.parametrize(
     'es_data, query_data, expected_answer', [
-        (ES_MOVIES_TEST_DATA, {'search': 'Star'}, {'status': 200, 'length': 50}),
-        (ES_MOVIES_TEST_DATA, {'search': 'Mashed potato'}, {'status': 404, 'length': 1})
+        (ES_MOVIES_TEST_DATA, {'query': 'Star'}, {'status': 200, 'length': 50}),
+        (ES_MOVIES_TEST_DATA, {'query': 'Mashed potato'}, {'status': 404, 'length': 1})
     ]
 )
 @pytest.mark.asyncio(scope='session')
 async def test_search(es_write_data, make_get_request, es_data: list[dict], query_data, expected_answer):
     await es_write_data(es_data, search_test_settings)
 
-    response = await make_get_request('/api/v1/films/search', query_data, search_test_settings)
+    response = await make_get_request('/api/v1/films/search', search_test_settings, **query_data)
 
     assert response['status'] == expected_answer['status']
     assert len(response['body']) == expected_answer['length']
