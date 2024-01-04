b00ee2efa955d8ea3554371517749d8a4e722d9b
Mikhail Shamanin <smb112@yandex.ru>
2024-01-04 13:35:39 +0300

    Использовать backoff в скриптах ожидания ES и Redis

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,11 +1,23 @@
+import logging
+
+import backoff
 import time
 
 from elasticsearch import Elasticsearch
+
 from tests.functional.settings import session_settings
 
-if __name__ == '__main__':
+
+@backoff.on_exception(backoff.expo,
+                      Exception,
+                      logger=logging.getLogger('backoff').addHandler(logging.StreamHandler()))
+def check_es_availability():
     es_client = Elasticsearch(hosts=session_settings.es_host)
-    while True:
-        if es_client.ping():
-            break
+    if not es_client.ping():
         time.sleep(1)
+        raise Exception
+    print('Es ping is ok')
+
+
+if __name__ == '__main__':
+    check_es_availability()
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,11 +1,22 @@
+import logging
+
+import backoff
 import time
 
 from redis import Redis
 from tests.functional.settings import session_settings
 
-if __name__ == '__main__':
+
+@backoff.on_exception(backoff.expo,
+                      Exception,
+                      logger=logging.getLogger('backoff').addHandler(logging.StreamHandler()))
+def check_redis_availability():
     redis_client = Redis(host=session_settings.redis_host, port=session_settings.redis_port)
-    while True:
-        if redis_client.ping():
-            break
+    if not redis_client.ping():
         time.sleep(1)
+        raise Exception
+    print('Redis ping is ok')
+
+
+if __name__ == '__main__':
+    check_redis_availability()
