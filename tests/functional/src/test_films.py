cbd86d8a89bded62b8a5be859bf76c9323e6635e
Mikhail Shamanin <smb112@yandex.ru>
2024-01-04 14:35:07 +0300

    Разделить логические единицы (AAA) в тестах. Разделить тесты согласно принципа SRP.

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -20,57 +20,102 @@ async def test_films_list(
         make_get_request):
     # 1. Подготовка данных.
     es_data = await prepare_index(generate_movies_index, es_write_data)
-
     (short_films_data, sorted_short_films_data, sorted_desc_short_films_data, genre_to_filter,
      filtered_short_films_data) = await get_films_test_data(es_data)
+    page_size = random.randint(10, 50)
 
+    # 2. Тестирование получения данных по API.
+    await redis_flush_db()
+    result = await get_all_records(film_test_settings, '/api/v1/films/', page_size, len(es_data))
+
+    assert len(result) == len(es_data)
+    assert result == short_films_data
+
+    # 3. Тестирование получение данных из кэша.
+    await es_drop_index(film_test_settings)
+    result = await get_all_records(film_test_settings, '/api/v1/films/', page_size, len(es_data))
+
+    assert len(result) == len(es_data)
+    assert result == short_films_data
+
+    # 4. Очистка.
+    await es_drop_index(film_test_settings)
+    await redis_flush_db()
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_films_list_sort(
+        es_write_data, es_drop_index, es_client, redis_flush_db, generate_movies_index, get_all_records,
+        make_get_request):
+    # 1. Подготовка данных.
+    es_data = await prepare_index(generate_movies_index, es_write_data)
+    (short_films_data, sorted_short_films_data, sorted_desc_short_films_data, genre_to_filter,
+     filtered_short_films_data) = await get_films_test_data(es_data)
+    page_size = random.randint(10, 50)
+
+    # 2. Тестирование получения данных по API.
+    await redis_flush_db()
+    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=imdb_rating', page_size, len(es_data))
+
+    assert result == sorted_short_films_data
+    assert len(result) == len(sorted_short_films_data)
+
+    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=-imdb_rating', page_size, len(es_data))
+
+    assert result == sorted_desc_short_films_data
+    assert len(result) == len(sorted_desc_short_films_data)
+
+    # 3. Тестирование получение данных из кэша.
+    await es_drop_index(film_test_settings)
+    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=imdb_rating', page_size, len(es_data))
+
+    assert result == sorted_short_films_data
+    assert len(result) == len(sorted_short_films_data)
+
+    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=-imdb_rating', page_size, len(es_data))
+
+    assert result == sorted_desc_short_films_data
+    assert len(result) == len(sorted_desc_short_films_data)
+
+    # 4. Очистка.
+    await es_drop_index(film_test_settings)
+    await redis_flush_db()
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_films_list_filter(
+        es_write_data, es_drop_index, es_client, redis_flush_db, generate_movies_index, get_all_records,
+        make_get_request):
+    # 1. Подготовка данных.
+    es_data = await prepare_index(generate_movies_index, es_write_data)
+    (short_films_data, sorted_short_films_data, sorted_desc_short_films_data, genre_to_filter,
+     filtered_short_films_data) = await get_films_test_data(es_data)
     absent_genre = await get_absent_genre()
 
     page_size = random.randint(10, 50)
 
     # 2. Тестирование получения данных по API.
     await redis_flush_db()
-
-    result = await get_all_records(film_test_settings, '/api/v1/films/', page_size, len(es_data))
-    assert len(result) == len(es_data)
-    assert result == short_films_data
-
-    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=imdb_rating', page_size, len(es_data))
-    assert result == sorted_short_films_data
-    assert len(result) == len(sorted_short_films_data)
-
-    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=-imdb_rating', page_size, len(es_data))
-    assert result == sorted_desc_short_films_data
-    assert len(result) == len(sorted_desc_short_films_data)
-
     result = await get_all_records(film_test_settings, f'/api/v1/films/?genre={genre_to_filter}', page_size,
                                    len(es_data))
+
     assert result == filtered_short_films_data
     assert len(result) == len(filtered_short_films_data)
 
     response = await make_get_request(f'/api/v1/films/?genre={absent_genre}', film_test_settings)
+
     assert response['status'] == HTTPStatus.NOT_FOUND
 
     # 3. Тестирование получение данных из кэша.
     await es_drop_index(film_test_settings)
-    result = await get_all_records(film_test_settings, '/api/v1/films/', page_size, len(es_data))
-    assert len(result) == len(es_data)
-    assert result == short_films_data
-
-    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=imdb_rating', page_size, len(es_data))
-    assert result == sorted_short_films_data
-    assert len(result) == len(sorted_short_films_data)
-
-    result = await get_all_records(film_test_settings, '/api/v1/films/?sort=-imdb_rating', page_size, len(es_data))
-    assert result == sorted_desc_short_films_data
-    assert len(result) == len(sorted_desc_short_films_data)
-
     result = await get_all_records(film_test_settings, f'/api/v1/films/?genre={genre_to_filter}', page_size,
                                    len(es_data))
+
     assert result == filtered_short_films_data
     assert len(result) == len(filtered_short_films_data)
 
     response = await make_get_request(f'/api/v1/films/?genre={absent_genre}', film_test_settings)
+
     assert response['status'] == HTTPStatus.NOT_FOUND
 
     # 4. Очистка.
@@ -82,6 +127,7 @@ async def check_valid_requests_film_get(film_ids, film_index, make_get_request):
     for ind in film_ids:
         film = film_index[ind]
         response = await make_get_request(f'/api/v1/films/{film["uuid"]}', film_test_settings)
+
         assert response['status'] == HTTPStatus.OK
         assert response['body'] == film
 
@@ -90,19 +136,21 @@ async def check_valid_requests_film_get(film_ids, film_index, make_get_request):
 async def test_films_get(es_write_data, redis_flush_db, make_get_request, es_drop_index, generate_movies_index):
     # 1. Подготовка данных.
     film_index = await prepare_index(generate_movies_index, es_write_data)
-
     film_ids = [random.randint(0, len(film_index) - 1) for _ in range(1, 20)]
     invalid_uuids = [str(uuid.uuid4()) for _ in range(1, 20)]
 
     # 2. Тестирование получения неверных данных по API.
     for tid in invalid_uuids:
         response = await make_get_request(f'/api/v1/films/{tid}', film_test_settings)
+
         assert response['status'] == HTTPStatus.NOT_FOUND
 
     # 3. Тестирование получения верных данных по API.
     await redis_flush_db()
+
     await check_valid_requests_film_get(film_ids, film_index, make_get_request)
 
     # 4. Тестирование получения верных данных из кэша.
     await es_drop_index(film_test_settings)
+
     await check_valid_requests_film_get(film_ids, film_index, make_get_request)
--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -20,69 +20,72 @@ async def prepare_movies_index(es_write_data):
 
 
 @pytest.mark.asyncio(scope="session")
-async def test_films_search(
+async def test_films_search_empty_query(
         es_write_data, es_drop_index, es_client, redis_flush_db, generate_movies_index, get_all_records):
 
     # 1. Подготовка данных.
     es_data = await prepare_movies_index(es_write_data)
-
     (word_to_search, absent_word, short_films_data, searched_short_films_data, searched_sorted_short_films_data,
      searched_sorted_desc_short_films_data) = await get_films_search_test_data(es_data)
-
     page_size = random.randint(10, 50)
 
     # 2. Тестирование получения данных по API.
     await redis_flush_db()
 
     result = await get_all_records(film_test_settings, '/api/v1/films/search/', page_size, len(short_films_data))
+
     assert len(result) == len(short_films_data)
     assert result == short_films_data
-
-    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}', page_size,
-                                   len(es_data))
-    assert len(result) == len(searched_short_films_data)
-    assert sorted(result, key=(lambda film: film.get('title'))) == searched_short_films_data
-
-    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}&sort=imdb_rating',
-                                   page_size, len(es_data))
-    assert result == searched_sorted_short_films_data
-    assert len(result) == len(searched_sorted_short_films_data)
-
-    result = await get_all_records(film_test_settings,
-                                   f'/api/v1/films/search/?query={word_to_search}&sort=-imdb_rating', page_size,
-                                   len(es_data))
-    assert result == searched_sorted_desc_short_films_data
-    assert len(result) == len(searched_sorted_desc_short_films_data)
-
-    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={absent_word}', page_size,
-                                   len(es_data))
-    assert len(result) == 0
 
     # 3. Тестирование получение данных из кэша.
     await es_drop_index(film_test_settings)
 
     result = await get_all_records(film_test_settings, '/api/v1/films/search/', page_size, len(short_films_data))
+
     assert len(result) == len(short_films_data)
     assert result == short_films_data
 
+    # 4. Очистка.
+    await es_drop_index(film_test_settings)
+    await redis_flush_db()
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_films_search_query(
+        es_write_data, es_drop_index, es_client, redis_flush_db, generate_movies_index, get_all_records):
+
+    # 1. Подготовка данных.
+    es_data = await prepare_movies_index(es_write_data)
+    (word_to_search, absent_word, short_films_data, searched_short_films_data, searched_sorted_short_films_data,
+     searched_sorted_desc_short_films_data) = await get_films_search_test_data(es_data)
+    page_size = random.randint(10, 50)
+
+    # 2. Тестирование получения данных по API.
+    await redis_flush_db()
+
     result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}', page_size,
                                    len(es_data))
+
     assert len(result) == len(searched_short_films_data)
     assert sorted(result, key=(lambda film: film.get('title'))) == searched_short_films_data
 
-    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}&sort=imdb_rating',
-                                   page_size, len(es_data))
-    assert result == searched_sorted_short_films_data
-    assert len(result) == len(searched_sorted_short_films_data)
-
-    result = await get_all_records(film_test_settings,
-                                   f'/api/v1/films/search/?query={word_to_search}&sort=-imdb_rating', page_size,
+    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={absent_word}', page_size,
                                    len(es_data))
-    assert result == searched_sorted_desc_short_films_data
-    assert len(result) == len(searched_sorted_desc_short_films_data)
+
+    assert len(result) == 0
+
+    # 3. Тестирование получение данных из кэша.
+    await es_drop_index(film_test_settings)
+
+    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}', page_size,
+                                   len(es_data))
+
+    assert len(result) == len(searched_short_films_data)
+    assert sorted(result, key=(lambda film: film.get('title'))) == searched_short_films_data
 
     result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={absent_word}', page_size,
                                    len(es_data))
+
     assert len(result) == 0
 
     # 4. Очистка.
@@ -91,46 +94,116 @@ async def test_films_search(
 
 
 @pytest.mark.asyncio(scope="session")
-async def test_persons_search(
+async def test_films_search_query_sorted(
+        es_write_data, es_drop_index, es_client, redis_flush_db, generate_movies_index, get_all_records):
+    # 1. Подготовка данных.
+    es_data = await prepare_movies_index(es_write_data)
+    (word_to_search, absent_word, short_films_data, searched_short_films_data, searched_sorted_short_films_data,
+     searched_sorted_desc_short_films_data) = await get_films_search_test_data(es_data)
+    page_size = random.randint(10, 50)
+
+    # 2. Тестирование получения данных по API.
+    await redis_flush_db()
+
+    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}&sort=imdb_rating',
+                                   page_size, len(es_data))
+
+    assert result == searched_sorted_short_films_data
+    assert len(result) == len(searched_sorted_short_films_data)
+
+    result = await get_all_records(film_test_settings,
+                                   f'/api/v1/films/search/?query={word_to_search}&sort=-imdb_rating', page_size,
+                                   len(es_data))
+
+    assert result == searched_sorted_desc_short_films_data
+    assert len(result) == len(searched_sorted_desc_short_films_data)
+
+    # 3. Тестирование получение данных из кэша.
+    await es_drop_index(film_test_settings)
+
+    result = await get_all_records(film_test_settings, f'/api/v1/films/search/?query={word_to_search}&sort=imdb_rating',
+                                   page_size, len(es_data))
+
+    assert result == searched_sorted_short_films_data
+    assert len(result) == len(searched_sorted_short_films_data)
+
+    result = await get_all_records(film_test_settings,
+                                   f'/api/v1/films/search/?query={word_to_search}&sort=-imdb_rating', page_size,
+                                   len(es_data))
+
+    assert result == searched_sorted_desc_short_films_data
+    assert len(result) == len(searched_sorted_desc_short_films_data)
+
+    # 4. Очистка.
+    await es_drop_index(film_test_settings)
+    await redis_flush_db()
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_persons_search_empty_query(
         es_write_data, es_drop_index, es_client, redis_flush_db, generate_persons_index, get_all_records,
         make_get_request, generate_movies_index):
     # 1. Подготовка данных.
     persons_index = await prepare_persons_index(es_write_data)
-
     (word_to_search, absent_word, searched_persons_data) = await get_persons_search_test_data(persons_index)
-
     page_size = random.randint(10, 50)
 
     # 2. Тестирование получения данных по API.
     await redis_flush_db()
 
     result = await get_all_records(person_test_settings, '/api/v1/persons/search/', page_size, len(persons_index))
+
     assert len(result) == len(persons_index)
     assert result == persons_index
-
-    result = await get_all_records(film_test_settings, f'/api/v1/persons/search/?query={word_to_search}', page_size,
-                                   len(persons_index))
-    assert len(result) == len(searched_persons_data)
-    assert sorted(result, key=(lambda film: film.get('full_name'))) == searched_persons_data
-
-    result = await get_all_records(film_test_settings, f'/api/v1/persons/search/?query={absent_word}', page_size,
-                                   len(persons_index))
-    assert len(result) == 0
 
     # 3. Тестирование получение данных из кэша.
     await es_drop_index(film_test_settings)
 
     result = await get_all_records(person_test_settings, '/api/v1/persons/search/', page_size, len(persons_index))
+
     assert len(result) == len(persons_index)
     assert result == persons_index
 
+    # 4. Очистка.
+    await es_drop_index(film_test_settings)
+    await redis_flush_db()
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_persons_search_query(
+        es_write_data, es_drop_index, es_client, redis_flush_db, generate_persons_index, get_all_records,
+        make_get_request, generate_movies_index):
+    # 1. Подготовка данных.
+    persons_index = await prepare_persons_index(es_write_data)
+    (word_to_search, absent_word, searched_persons_data) = await get_persons_search_test_data(persons_index)
+    page_size = random.randint(10, 50)
+
+    # 2. Тестирование получения данных по API.
+    await redis_flush_db()
+
+    result = await get_all_records(film_test_settings, f'/api/v1/persons/search/?query={word_to_search}', page_size,
+                                   len(persons_index))
+
+    assert len(result) == len(searched_persons_data)
+    assert sorted(result, key=(lambda film: film.get('full_name'))) == searched_persons_data
+
+    result = await get_all_records(film_test_settings, f'/api/v1/persons/search/?query={absent_word}', page_size,
+                                   len(persons_index))
+
+    assert len(result) == 0
+
+    # 3. Тестирование получение данных из кэша.
+    await es_drop_index(film_test_settings)
+
     result = await get_all_records(person_test_settings, f'/api/v1/persons/search/?query={word_to_search}', page_size,
                                    len(persons_index))
+
     assert len(result) == len(searched_persons_data)
     assert sorted(result, key=(lambda film: film.get('full_name'))) == searched_persons_data
 
     result = await get_all_records(person_test_settings, f'/api/v1/persons/search/?query={absent_word}', page_size,
                                    len(persons_index))
+
     assert len(result) == 0
 
     # 4. Очистка.
